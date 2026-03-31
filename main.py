3ab049cea6b6e51e273d846a000facf03349a9df
ih3421 <ismail.hussain2302@gmail.com>
2026-04-01 01:31:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+import jwt
 app = Flask(__name__)
 db = SQLAlchemy(app)
+key='secret'
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # autoincrement=True is default
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(30))
+    name = db.Column(db.String(60))
 
 @app.route("/login",methods=['POST'])
 def login():
@@ -10,6 +17,36 @@ def login():
     user_name = data['username']
     password = hash(data['password'])
     user = User.query.filter_by(username=user_name)
-    if user.password== password:
+    if user ==None:
+    return jsonify({'error': 'Invalid credentials'}),401
+    if user.password == password:
+        payload={ user:user_name,
+                 iat: int(datetime.utcnow().timestamp()),
+                 exp: int((datetime.utcnow() + timedelta(hours=1)).timestamp())}
+        token=jwt.encode(payload,key)
+        return jsonify({'token':token,'status':200})
+    return jsonify({'error': 'Invalid credentials'}),401
+
+@app.route("/reg",methods=['POST'])
+def register:
+    data = request.json
+    name = data['name']        
+    user_name = data['username']
+    password = hash(data['password'])
+    user = User.query.filter_by(username=user_name)
+    if user:
+        return jsonify({'error': 'Username taken'}),400
+    user = User(
+        username = user_name
+        name = name
+        password = password)
+    db.session.add(user)      # Stage for INSERT
+    db.session.commit()       # Execute SQL
     
-    return 
+    return jsonify({'message': 'User created', 'id': user.id}), 201
+
+
+    )
+    
+
+
